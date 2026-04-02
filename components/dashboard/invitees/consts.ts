092a30abccd7eff0b274afaf91a336c027701811
Nicolas Sanchez <nicosanchez673@gmail.com>
2026-04-02 12:01:17 -0300

    update for ivan

--- components/dashboard/invitees/consts.ts
+++ components/dashboard/invitees/consts.ts
@@ -1,4 +1,4 @@
-const productionPath = "https://invitee-nico.vercel.app";
+const productionPath = "https://invitee-ivan.vercel.app";
 const developmentPath = "http://localhost:3000";
 
 export const invitationText = "Buenas! Espero estés bien! Voy a festejar mi cumpleaños y me gustaría que estés ahí!\n\nHice una app para que puedas confirmar tu asistencia, ahí mismo se encuentran los detalles, espero que te guste!\nLink: ";

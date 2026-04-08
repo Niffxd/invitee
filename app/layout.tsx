b278beecfe8f16a12663cb2ce447babfdd5c072f
Nicolas Sanchez <nicosanchez673@gmail.com>
2026-04-08 20:25:58 -0300

    udpate for Ivan and Eve

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html className="light" data-theme="light" suppressHydrationWarning>
+    <html className="light" data-theme="light" suppressHydrationWarning lang="es">
       <body className="bg-background text-foreground">
         <SpeedInsights />
         <Toast.Container placement="bottom" />
